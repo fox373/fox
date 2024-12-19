8571a96afa6e5ba4736d6d71b3e95391ff766454
fox373 <202483920035@nuist.edu.cn>
2024-12-19 12:19:30 +0000

    format ...

--- src/main/java/com/bjsxt/Person.java
+++ src/main/java/com/bjsxt/Person.java
@@ -71,14 +71,19 @@ public class Person {
     }
 
     @Override
+    // public String toString() {
+    // StringBuffer sb = new StringBuffer();
+    // sb.append("id").append(this.id).append("#").append("\t");
+    // sb.append("name").append(this.name).append("\t\t");
+    // sb.append("age").append(this.age).append("\t\t");
+    // sb.append("sex").append(this.sex).append("\t\t");
+    // sb.append("phone number").append(this.telNum).append("\t\t");
+    // sb.append("address").append(this.address);
+    // return sb.toString();
+    // }
     public String toString() {
-        StringBuffer sb = new StringBuffer();
-        sb.append("id").append(this.id).append("#").append("\t");
-        sb.append("name").append(this.name).append("\t\t");
-        sb.append("age").append(this.age).append("\t\t");
-        sb.append("sex").append(this.sex).append("\t\t");
-        sb.append("phone number").append(this.telNum).append("\t\t");
-        sb.append("address").append(this.address);
-        return sb.toString();
+        // 使用 String.format 来格式化输出并确保每个字段对齐
+        return String.format("id: %-5d name: %-20s age: %-3s sex: %-5s phone number: %-15s address: %-30s",
+                this.id, this.name, this.age, this.sex, this.telNum, this.address);
     }
 }
